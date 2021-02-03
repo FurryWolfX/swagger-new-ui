36b0ae2814ebcc651af7ab56c7aa3ad90f2abba0
wolfx <digimonx@qq.com>
2021-02-03 11:26:17 +0800

    可以保存常用地址

--- src/app/App.SearchBar.tsx
+++ src/app/App.SearchBar.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useState } from "react";
-import { Button, Col, Input, Row, AutoComplete } from "antd";
+import { Button, Col, Input, Row, AutoComplete, message } from "antd";
+import uniq from "lodash/uniq";
+import uniqBy from "lodash/uniqBy";
 import { observer } from "mobx-react-lite";
 import { getData } from "./App.utils";
 import store from "./App.store";
@@ -12,10 +14,24 @@ const options = [
   { value: "http://192.168.1.5:8089/workflow" }
 ];
 
+function saveRemoteUrl(remoteUrl: string) {
+  const userRemoteUrls: string[] = JSON.parse(window.localStorage.getItem("userRemoteUrls") || "[]");
+  userRemoteUrls.push(remoteUrl);
+  window.localStorage.setItem("userRemoteUrls", JSON.stringify(uniq(userRemoteUrls)));
+  message.info("保存成功");
+}
+
+function getRemoteUrlOption() {
+  const userRemoteUrls: string[] = JSON.parse(window.localStorage.getItem("userRemoteUrls") || "[]");
+  userRemoteUrls.reverse().forEach(url => {
+    options.unshift({ value: url });
+  });
+  return uniqBy(options, item => item.value);
+}
+
 function SearchBar() {
-  const [remoteUrl, setRemoteUrl] = useState(
-    "http://192.168.1.5:9999/admin-api"
-  );
+  const remoteUrlOption = getRemoteUrlOption();
+  const [remoteUrl, setRemoteUrl] = useState("http://192.168.1.5:9999/admin-api");
   const [loading, setLoading] = useState(false);
   const getDataAsync = useCallback(async () => {
     setLoading(true);
@@ -24,26 +40,27 @@ function SearchBar() {
   }, [remoteUrl]);
   return (
     <>
-      <div>为难以检索的Swagger重新写了个新界面（v3.0）By wolfx.cn</div>
+      <div>为难以检索的Swagger重新写了个新界面（v3.1.0）By wolfx.cn</div>
       <Row>
         <Col span={12}>
           <span style={{ verticalAlign: "middle" }}>
             <AutoComplete
-              options={options}
+              options={remoteUrlOption}
               onChange={ev => setRemoteUrl(ev)}
               value={remoteUrl}
               placeholder="请输入服务端地址"
             >
-              <Input
-                style={{ width: 400 }}
-                value={remoteUrl}
-                onInput={ev => setRemoteUrl(ev.currentTarget.value)}
-              />
+              <Input style={{ width: 400 }} value={remoteUrl} onInput={ev => setRemoteUrl(ev.currentTarget.value)} />
             </AutoComplete>
           </span>
           <span style={{ verticalAlign: "middle", paddingLeft: 20 }}>
-            <Button loading={loading} onClick={() => getDataAsync()}>
+            <Button type={"primary"} loading={loading} onClick={() => getDataAsync()}>
               解析
+            </Button>
+          </span>
+          <span style={{ verticalAlign: "middle", paddingLeft: 20 }}>
+            <Button loading={loading} onClick={() => saveRemoteUrl(remoteUrl)}>
+              保存常用地址
             </Button>
           </span>
         </Col>
